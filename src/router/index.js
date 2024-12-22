0625972cb42ccd0998c042fc9da83118cbec1113
byteflipper.business <byteflipper.business@gmail.com>
2024-12-22 22:46:12 +0000

    Added page for: EverBook

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,11 @@ const routes = [
     component: () => import('@/views/FFSettingsTermsConditions.vue'),
   },
   {
+    path: '/everbook',
+    name: 'Everbook',
+    component: () => import('@/views/Everbook.vue'),
+  },
+  {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: () => import('@/views/NotFound.vue'),
